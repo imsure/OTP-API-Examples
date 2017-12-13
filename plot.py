f1d0ab278c2b1fab1f62fee0020f5d8a6fb2b614
Shuo Yang <imsure95@gmail.com>
2017-12-13 15:57:50 -0700

    update

--- plot.py
+++ plot.py
@@ -2,26 +2,29 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-data = np.loadtxt('runtime_seq_1000.txt', delimiter=' ')
+base_dir = 'data_plots'
+
+data = np.loadtxt(os.path.join(base_dir, 'run_ec2_seq_1000.txt'), delimiter=' ')
 times = data[:,1]
 avg = np.mean(times)
 fig = plt.figure()
 plt.hist(times, bins=50, color='y')
 plt.xlabel('OTP Server Response Time (in ms)')
 plt.ylabel('# of Hits')
-plt.title('1000 API Requests Issued Sequentially\nAverage Response Time: {0:.2f} ms'.format(avg))
-fig.savefig('runtime_seq_1000.png')
+plt.title('1000 API Requests Issued Sequentially from a EC2 instance\nAverage Response Time: {0:.2f} ms'.format(avg))
+fig.savefig(os.path.join(base_dir, 'runtime_ec2_seq_1000.png'))
 print ('run seq 1000:', avg)
 
-for i in range(1,7):
-    data = np.loadtxt('run{}.txt'.format(i), delimiter=' ')
+for i in range(1,4):
+    data = np.loadtxt(os.path.join(base_dir, 'run_ec2_con_10000_{}.txt'.format(i)), delimiter=' ')
     times = data[:,2]
     avg = np.mean(times)
     fig = plt.figure()
     plt.hist(times, bins=50, color='y')
     plt.xlabel('OTP Server Response Time (in ms)')
     plt.ylabel('# of Hits')
-    plt.title('1000 API Requests Issued Concurrently\nAverage Response Time: {0:.2f} ms'.format(avg))
-    fig.savefig('run{}.png'.format(i))
+    plt.title('10000 API Requests Issued Concurrently from a EC2 instance\nAverage Response Time: {0:.2f} ms'.format(avg))
+    fig.savefig(os.path.join(base_dir, 'run_ec2_con_10000_{}.png'.format(i)))
     print ('run {}:'.format(i), avg)
